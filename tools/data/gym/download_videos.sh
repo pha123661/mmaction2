2b6dec4de03aca9b0fa7503845ddca57ba9d6454
Oscar Li <pha123661@gmail.com>
2023-08-25 20:56:25 +0800

    Update ytdl

--- tools/data/gym/download_videos.sh
+++ tools/data/gym/download_videos.sh
@@ -4,7 +4,7 @@
 conda env create -f environment.yml
 source activate gym
 pip install mmengine
-pip install --upgrade youtube-dl
+pip install --upgrade --force-reinstall "git+https://github.com/ytdl-org/youtube-dl.git"
 
 DATA_DIR="../../../data/gym"
 ANNO_DIR="../../../data/gym/annotations"
