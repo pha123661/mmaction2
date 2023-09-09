846cfa6efe41340867677aa716ad22513322905d
Li, Sheng-Wei <pha123661@gmail.com>
2023-09-09 14:50:44 +0800

    Add download report

--- tools/data/gym/download_videos.sh
+++ tools/data/gym/download_videos.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 
 # set up environment
-conda env create -f environment.yml
-source activate gym
-pip install mmengine
-pip install --upgrade --force-reinstall "git+https://github.com/ytdl-org/youtube-dl.git"
+# conda env create -f environment.yml
+# source activate gym
+# pip install mmengine
+# pip install --upgrade --force-reinstall "git+https://github.com/ytdl-org/youtube-dl.git"
 
 DATA_DIR="../../../data/gym"
 ANNO_DIR="../../../data/gym/annotations"
 python download.py ${ANNO_DIR}/annotation.json ${DATA_DIR}/videos
 
-source deactivate gym
-conda remove -n gym --all
+# source deactivate gym
+# conda remove -n gym --all
